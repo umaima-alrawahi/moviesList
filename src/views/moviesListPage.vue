f170ae76ca883f880193e519f4b1eaa97ab4aa0a
umaima-alrawahi <omaima97alrawahi@gmail.com>
2022-11-25 11:55:37 +0400

    loading in list

--- src/views/moviesListPage.vue
+++ src/views/moviesListPage.vue
@@ -1,10 +1,14 @@
 <script setup>
 import movieList from '../components/movieList.vue';
+import SimpleLoading from '../components/loading/SimpleLoading.vue';
 </script>
 <template>
   <Suspense>
     <movieList class="container" />
     <template #fallback>
+
+      <SimpleLoading class="container" />
+
     </template>
   </Suspense>
 </template>
